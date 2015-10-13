af013a9248163bca9b8569c2e9f842126af4cc9e
seboccis <seboccis@orange.fr>
2015-10-13 11:44:49 +0200

    final commit

--- presentation.php
+++ presentation.php
@@ -1,3 +1,3 @@
-<h3>Votre présence sur la toile est aujourd'hui indispensable et ne peut être efficace qu'accompagnée de professionnels.</br> 
+<p class="pitch">Votre présence sur la toile est aujourd'hui indispensable et ne peut être efficace qu'accompagnée de professionnels.</br> 
 	SebNa vous propose de définir ensemble le projet le plus adapté à votre secteur d'activité et vos moyens même pour des sites perso.</br> 
-	N'hésitez plus, contactez-nous gratuitement en utilisant ce formulaire de contact :</h3>
+	N'hésitez plus, contactez-nous gratuitement en utilisant ce formulaire de contact :</p>
